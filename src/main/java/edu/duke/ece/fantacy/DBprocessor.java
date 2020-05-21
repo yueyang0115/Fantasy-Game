3ffa951843b0ef3ea47ef9bd860dcec6bb56bae4
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-05-20 20:48:35 -0400

    add territoryHandler

---
+++ src/main/java/edu/duke/ece/fantacy/DBprocessor.java
@@ -0,0 +1,10 @@
+package edu.duke.ece.fantacy;
+
+public class DBprocessor {
+    void addTerritory(int wid,double x,double y,String status){
+
+    }
+    void updateTerritory(int wid,double x,double y,String status){}
+    boolean checkTerritory(int wid,double x,double y){return false;}
+    Territory getTerritory(int wid,double x,double y){return null;}
+}
---
+++ src/main/java/edu/duke/ece/fantacy/Territory.java
@@ -0,0 +1,4 @@
+package edu.duke.ece.fantacy;
+
+public class Territory {
+}
