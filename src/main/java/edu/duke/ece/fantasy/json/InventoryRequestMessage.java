734710cffacf58c5dc80958d10929b14e6a909a7
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-06-07 16:09:11 -0400

    sync for inventoryrequest

--- src/main/java/edu/duke/ece/fantasy/json/InventoryRequestMessage.java
+++ src/main/java/edu/duke/ece/fantasy/json/InventoryRequestMessage.java
@@ -1,6 +1,20 @@
 package edu.duke.ece.fantasy.json;
 
 public class InventoryRequestMessage {
+    private String action;
+
     public InventoryRequestMessage() {
+    }
+
+    public InventoryRequestMessage(String action) {
+        this.action = action;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
     }
 }
