9976d95d4d691a0073dcb165084a59af9acff610
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-06-23 15:40:20 -0400

    return the player's inventory order by id

--- src/main/java/edu/duke/ece/fantasy/building/BaseShop.java
+++ src/main/java/edu/duke/ece/fantasy/building/BaseShop.java
@@ -2,6 +2,7 @@ package edu.duke.ece.fantasy.building;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import edu.duke.ece.fantasy.Item.Potion;
+import edu.duke.ece.fantasy.Item.SuperPotion;
 import edu.duke.ece.fantasy.database.*;
 import org.hibernate.Session;
 
@@ -14,6 +15,7 @@ public class BaseShop extends Shop {
         super("shop", 200);
         shopInventory potion_Inventory = new shopInventory(new Potion().toDBItem(), 20);
         possible_inventory.add(potion_Inventory);
+        possible_inventory.add(new shopInventory(new SuperPotion().toDBItem(), 20));
         Shop super_shop = new SuperShop();
         UpgradeTo.put(super_shop.getName(), super_shop);
     }
