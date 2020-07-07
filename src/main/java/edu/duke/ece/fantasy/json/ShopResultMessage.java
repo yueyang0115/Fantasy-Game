d80cb312a5296ba38aaed21868f280218282100f
Yunzhe Jia <yj@MBP-Yunzhe.local>
2020-07-07 13:55:23 -0400

    delete inventoryResult in shopResult

--- src/main/java/edu/duke/ece/fantasy/json/ShopResultMessage.java
+++ src/main/java/edu/duke/ece/fantasy/json/ShopResultMessage.java
@@ -8,19 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShopResultMessage {
-    private InventoryResultMessage inventoryResultMessage;
     private List<Inventory> items = new ArrayList<>(); //all items in the shop
     private String result; //status: "valid","invalid"
 
     public ShopResultMessage() {
-    }
-
-    public InventoryResultMessage getInventoryResultMessage() {
-        return inventoryResultMessage;
-    }
-
-    public void setInventoryResultMessage(InventoryResultMessage inventoryResultMessage) {
-        this.inventoryResultMessage = inventoryResultMessage;
     }
 
     public List<Inventory> getItems() {
